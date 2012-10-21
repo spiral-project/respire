8d9a6919b80a0cf7578b2e3cdeb4d79874e0e15e
Alexis Métaireau <alexis@notmyidea.org>
2012-10-22 01:59:10 +0200

    Document and refactor the implementation

--- respire/client.py
+++ respire/client.py
@@ -15,59 +15,104 @@ def client_from_url(url):
     return Client(description=schema)
 
 
-def make_spore_function(definition, description):
+def make_spore_function(method_definition, service_description):
+    """Returns the actual function being exposed to the end user.
+
+    Handles the actual call to the resource and define for you some
+    additional headers and behaviour depending on the spore definition that was
+    given.
+
+    :param method_definition:
+        Definition of the method we are defining the function.
+
+    :param service_description:
+        SPORE description of the service. Could be useful to get top-level
+        information, such as the base url of the service.
+    """
+    # We need this level of indirection to handle the closures the right way.
+    # That's because we're defining the functions sometimes in a loop, and here
+    # we're defining a stack level for the passed arguments.
     def spore_function(raise_for_status=True, **method_kw):
-        params = []
-        for key in ('required_params', 'optional_params'):
-            if hasattr(definition, key):
-                params.extend(definition[key])
-
-        # now do the actual request
-        path = definition.path
-
         # for each param passed to the method,
         # match if it's needed in the path, and replace it there if
         # needed
+
+        path = method_definition.path
         for kw in method_kw.keys():
             key = ':%s' % kw
             if key in path:
                 path.replace(key, method_kw.pop(kw))
 
-        url = urljoin(description.base_url, path)
-        if 'headers' not in method_kw\
-            or 'Content-Type' not in method_kw['headers']:
-            if definition.format == 'json':
-                method_kw['headers'] = {'Content-Type': 'application/json'}
-        if definition.format == 'json' and 'data' in method_kw:
-            method_kw['data'] = json.dumps(method_kw['data'])
+        url = urljoin(service_description.base_url, path)
 
-        resp = requests.request(definition.method, url, **method_kw)
+        define_format(method_kw, method_definition)
+
+        # make the actual query to the resource
+        resp = requests.request(method_definition.method, url, **method_kw)
         if raise_for_status:
             resp.raise_for_status()
-        if resp.json:
-            return resp.json
-        return resp.body
 
+        return decode_response(resp, method_definition)
+
+    spore_function.__doc__ = get_method_documentation(method_definition)
     return spore_function
 
 
-class Client(object):
+def decode_response(resp, definition):
+    """Decode the response if we know how to handle it"""
+    if definition.format == 'json':
+        if resp.json:
+            return resp.json
+    return resp.body
 
-    def __init__(self, description):
-        self.description = EasyDict(description)
 
-        # now we want to build the methods
-        for method, definition in self.description.methods.items():
-            spore_function = make_spore_function(definition, self.description)
-            if 'description' not in definition:
-                definition['description'] = ''
+def define_format(kw, definition):
+    """Set the correct Content-Type headers and encode the data to the right
+    format, if we know how to handle it.
+    """
+    if definition.format == 'json':
+        if 'headers' not in kw or 'Content-Type' not in kw['headers']:
+            kw['headers'] = {'Content-Type': 'application/json'}
 
-            documentation = """
+        if 'data' in kw:
+            kw['data'] = json.dumps(kw['data'])
+    # XXX deal with other formats
+
+
+def get_method_documentation(definition):
+    """Get the documentation from the SPORE format and attach it to the method
+    that will be exposed to the user.
+
+    :param definition: the definition of the method, from the SPORE file.
+    """
+    if 'description' not in definition:
+        definition['description'] = ''
+
+    documentation = """
 {description}
 
 This binds to the {path} method.
 Output format is {format}.
 """.format(**definition)
+    return documentation
 
-            spore_function.__doc__ = documentation
+
+class Client(object):
+    """The way to interact with the API.
+
+    :param description:
+        a python object containing the definition of the SPORE service
+
+    This client provides two main things: the description, available as
+    a dotted dict, and a number of methods to interact with the service.
+    """
+
+    def __init__(self, description):
+        self.description = EasyDict(description)
+
+        # for each method defined in the spore file, create a method on this
+        # object.
+        for method, definition in self.description.methods.items():
+            spore_function = make_spore_function(definition, self.description)
+            spore_function.__name__ = method
             setattr(self, method, spore_function)
